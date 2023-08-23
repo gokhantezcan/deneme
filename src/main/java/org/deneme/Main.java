3f54d263f4e24d9130cde98e2eef5a34e568dff1
gokhantezcan <gokhant.41@gmail.com>
2023-08-24 00:40:36 +0300

    feature-2: shelve gokhan

--- src/main/java/org/deneme/Main.java
+++ src/main/java/org/deneme/Main.java
@@ -3,6 +3,7 @@ package org.deneme;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("shelve gokhan");
 
     }
 }
